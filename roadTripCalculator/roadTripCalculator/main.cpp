20f236d0221e0bb2540db2001b4f8b6fec231090
brandonmountan <brandonmountan1@gmail.com>
2024-08-20 10:55:51 -0600

    in class example

--- roadTripCalculator/roadTripCalculator/main.cpp
+++ roadTripCalculator/roadTripCalculator/main.cpp
@@ -9,6 +9,21 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    // defining initial inputs
+    int distance;
+    int mpg;
+    float gasPrice;
+    // asking for initial inputs
+    std::cout << "Enter driving distance in miles as a whole number \n";
+    std::cin >> distance;
+    std::cout << "Enter your car's miles per gallon to nearest whole number \n";
+    std::cin >> mpg;
+    std::cout << "Enter cost of gas per gallon to nearest penny \n";
+    std::cin >> gasPrice;
+    // performing calculations
+    float gallons = distance / mpg;
+    float totalCost = gallons * gasPrice;
+    // output
+    std::cout << "Gallons used: " << gallons << "\nTotal cost of gas: " << totalCost << "$" << "\n";
     return 0;
 }
