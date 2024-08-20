4b5d8b4dbf14e35820300d34239d06996f0afcc4
brandonmountan <brandonmountan1@gmail.com>
2024-08-20 12:28:38 -0600

    finished homework 2

--- Day2/GiveChange/GiveChange/main.cpp
+++ Day2/GiveChange/GiveChange/main.cpp
@@ -8,8 +8,31 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // defining initial inputs
+    int itemPrice, moneyInserted;
     
-    std::cout << "Hello, World!\n";
+    // input 1
+    std::cout << "Please enter price of item in pennies: ";
+    std::cin >> itemPrice;
+    
+    // input 2
+    std::cout << "Please enter amount of money deposited in pennies: ";
+    std::cin >> moneyInserted;
+    
+    // calculating based on inputs
+    int change = moneyInserted - itemPrice;
+    std::cout << "Change: " << change << " cents\n";
+    
+    
+    // modifying variable 'change' after
+    int quarters = change / 25;
+    change = change - (quarters * 25);
+    int dimes = change / 10;
+    change = change - (dimes * 10);
+    int nickels = change / 5;
+    change = change - (nickels * 5);
+    int pennies = change / 1;
+    
+    std:: cout << "Quarters: " << quarters << "\n" << "Dimes: " << dimes << "\n" << "Nickels: " << nickels << "\n" << "Pennies: " << pennies << "\n";
     return 0;
 }
