59ec818935c4d2c82f0c75057a5896fdd0b6a208
brandonmountan <brandonmountan1@gmail.com>
2024-08-21 10:07:37 -0600

    initial commit in class examples

---
+++ Day3/Day3InClassPractice/Day3InClassPractice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Day3InClassPractice
+//
+//  Created by Brandon Mountan on 8/21/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
