ee518d2a205f7b31d16bc286aa27ee30920a3aa9
brandonmountan <brandonmountan1@gmail.com>
2024-08-22 10:37:16 -0600

    password checker practice

--- Day4/Day4InClassPractice/Day4InClassPractice/main.cpp
+++ Day4/Day4InClassPractice/Day4InClassPractice/main.cpp
@@ -8,12 +8,24 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    std::string userInput;
+    std::string password;
+    bool isValid=false;
     
-    std::cout << "Enter some text \n";
-    std::cin >> userInput;
-    
-    std::cout << userInput << std::endl;
+    while(!isValid){
+        std::cout<<"please enter password \n";
+        std::cin>>password;
+        
+        if(password.length()<8){
+            std::cout<<"enter at least 8 characters \n";
+        } else if (password.find("$") == std::string::npos){
+            std::cout<<"please include '$' in password \n";
+        } else if (!(password[0]>='A' && password[0]<='Z')){
+            std::cout<<"please make first letter of password capitalized \n";
+        } else {
+            isValid=true;
+            std::cout<<"strong password \n";
+        }
+    }
     
     return 0;
 }
