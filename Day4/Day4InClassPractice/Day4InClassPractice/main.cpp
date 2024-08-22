67009feb08424594bf23d4bfa3a9441b25f4ff12
brandonmountan <brandonmountan1@gmail.com>
2024-08-22 09:34:11 -0600

    adding day4 folder

---
+++ Day4/Day4InClassPractice/Day4InClassPractice/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Day4InClassPractice
+//
+//  Created by Brandon Mountan on 8/22/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::string userInput;
+    
+    std::cout << "Enter some text \n";
+    std::cin >> userInput;
+    
+    std::cout << userInput << std::endl;
+    
+    return 0;
+}
