2d7ef5473f97364ffe7424d4d9943018d7d59393
brandonmountan <brandonmountan1@gmail.com>
2024-08-22 11:51:54 -0600

    deleted spaces

--- Day4/Palindromes/Palindromes/main.cpp
+++ Day4/Palindromes/Palindromes/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::string userInput;
+    
+    std::cout<<"please enter a palindrome using all lowercase letters \n";
+    
+    std::cin>>userInput;
+    
+    std::string reversed;
+    
+    for (int i=1; i<userInput.size()+1; i++){
+        reversed = reversed + userInput[userInput.length()-i];
+    }
+    
+    std::cout<<reversed<<std::endl;
+    
+    if (userInput==reversed){
+        std::cout<<"nice palindrome :)";
+    } else {
+        std::cout<<"not a palindrome :(";
+    }
+    
     return 0;
 }
+
