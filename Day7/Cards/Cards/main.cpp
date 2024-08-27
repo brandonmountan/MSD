a36a6296dfcb16275386a9d7220af488a8b3b6a6
brandonmountan <brandonmountan1@gmail.com>
2024-08-27 16:00:34 -0600

    finished up homework 6

---
+++ Day7/Cards/Cards/main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  Cards
+//
+//  Created by Brandon Mountan on 8/27/24.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+// spade = 1, club = 2, heart = 3, diamond = 4
+// rank: 1,2,3,4,5,6,7,8,9,10,11,12,13
+struct Cards {
+    int rank;
+    int suit;
+};
+
+std::vector<Cards> cardCreator(){
+    std::vector<Cards> container = {};
+    for (int rank = 1; rank < 14; rank++){
+        for (int suit = 1; suit < 5; suit++){
+            Cards card;
+            card.rank = rank;
+            card.suit = suit;
+            container.push_back(card);
+        }
+    }
+    return container;
+}
+
+int main(int argc, const char * argv[]) {
+
+//    Cards spade1;
+//    spade1.rank = 1;
+//    spade1.suit = 1;
+    
+    std::vector<Cards> deck = cardCreator();
+        
+    
+    for (int i = 0; i < deck.size(); i++){
+        char jack = 'J';
+        if (deck[i].rank == 11){
+            deck[i].rank = jack;
+        }
+        std::cout << deck[i].suit << " " << deck[i].rank << "\n";
+    }
+    
+    
+    return 0;
+}
