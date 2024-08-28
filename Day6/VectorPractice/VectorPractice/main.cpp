107e2196a26f8954d5dba35f62ca3013dacac6f2
brandonmountan <brandonmountan1@gmail.com>
2024-08-28 09:00:48 -0600

    finished vector practice lab finally

--- Day6/VectorPractice/VectorPractice/main.cpp
+++ Day6/VectorPractice/VectorPractice/main.cpp
@@ -19,8 +19,8 @@ int sum(std::vector<int> v){
 
 std::vector<char> stringToVec(std::string s){
     std::vector<char> result = {};
-    char c;
     for (int i = 0; i < s.size(); i++){
+        char c;
         c = s[i];
         result.push_back(c);
         std::cout << c << std::endl;
@@ -30,7 +30,10 @@ std::vector<char> stringToVec(std::string s){
 
 std::vector<int> reverse(std::vector<int> nums) {
     std::vector<int> results = {};
-
+    for (auto results = nums.rbegin(); results != nums.rend(); ++results) {
+        std::cout << *results << " ";
+    }
+    std::cout << std::endl;
     return results;
 }
 
@@ -43,7 +46,19 @@ int main(int argc, const char * argv[]) {
     
     std::string test = "hello";
     
-    stringToVec(test);
+    std::vector<char> newVec = stringToVec(test);
     
+    // iterating through the new vector to visualize its contents and test its functionality
+    for (int i = 0; i < newVec.size(); i++){
+        std::cout << newVec[i] << std::endl;
+    }
+    
+    std::vector<int> reverseContainer = reverse(nums);
+    
+    // iterating throught new vector of integers in reverse order to see its contents and test functionality
+    for (int i = 0; i < reverseContainer.size(); i++){
+        std::cout << reverseContainer[i] << std::endl;
+    }
+        
     return 0;
 }
