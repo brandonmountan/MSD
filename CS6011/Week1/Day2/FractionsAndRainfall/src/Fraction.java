542abbbbabd32bd5b525bf58a6a12fa5cdb09c3a
brandonmountan <brandonmountan1@gmail.com>
2024-09-25 14:26:00 -0600

    finishing fractions

--- CS6011/Week1/Day2/FractionsAndRainfall/src/Fraction.java
+++ CS6011/Week1/Day2/FractionsAndRainfall/src/Fraction.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.abs;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Fraction {
@@ -8,8 +10,8 @@ public class Fraction {
     long GCD() {
 // Returns the greatest common divisor of this fraction's numerator and denominator. This is a helper method for the reduce method, below.
 // Use the following iterative algorithm to compute the GCD:
-        long gcd = numerator;
-        long remainder = denominator;
+        long gcd = abs(numerator);
+        long remainder = abs(denominator);
         while (remainder != 0) {
             long temp = remainder;
             remainder = gcd % remainder;
@@ -26,72 +28,67 @@ public class Fraction {
     }
 
     // The default constructor, which sets the value of the fraction to 0/1. (Note, a constructor is similar to a method, but not actually a method.)
-    Fraction() {
+    public Fraction() {
         numerator = 0;
         denominator = 1;
     }
 
     // A constructor which sets the value of the fraction to a specific numerator (n) and denominator (d).
-    Fraction(long n, long d) {
+    public Fraction(long n, long d) {
         numerator = n;
         denominator = d;
-        if (d < 0 && n > 0) {
-            d = d * -1;
-            n = n * -1;
-        } else if (d < 0 && n < 0) {
-            d = d * -1;
-            n = n * -1;
+        if (d < 0) {
+            denominator = denominator * -1;
+            numerator = numerator * -1;
         }
         reduce();
     }
 
     // Returns a new fraction that is the result of the right hand side (rhs) fraction added to this fraction.
-    Fraction plus(Fraction rhs) {
+    public Fraction plus(Fraction rhs) {
         long num = (numerator * rhs.denominator) + (denominator * rhs.numerator);
         long den = (denominator * rhs.denominator);
         return new Fraction(num, den);
     }
 
     // Returns a new fraction that is the result of the right hand side (rhs) fraction subtracted from this fraction.
-    Fraction minus(Fraction rhs) {
+    public Fraction minus(Fraction rhs) {
         long num = (numerator * rhs.denominator) - (denominator * rhs.numerator);
         long den = (denominator * rhs.denominator);
         return new Fraction(num, den);
     }
 
     // Returns a new fraction that is the result of this fraction multiplied by the right hand side (rhs) fraction.
-    Fraction times(Fraction rhs) {
+    public Fraction times(Fraction rhs) {
         long num = numerator * rhs.numerator;
         long den = denominator * rhs.denominator;
         return new Fraction(num, den);
     }
 
     // Returns a new fraction that is the result of this fraction divided by the right hand side (rhs) fraction.
-    Fraction dividedBy(Fraction rhs) {
+    public Fraction dividedBy(Fraction rhs) {
         long num = numerator * rhs.denominator;
         long den = denominator * rhs.numerator;
         return new Fraction(num, den);
     }
 
     // Returns a new fraction that is the reciprocal of this fraction.
-    Fraction reciprocal() {
+    public Fraction reciprocal() {
         long num = denominator;
         long den = numerator;
         return new Fraction(num, den);
     }
 
     // Returns a string representing this fraction. The string should have the format: "N/D", where N is the numerator, and D is the denominator.
-    String fractionToString() {
-        String N = Long.toString(numerator);
-        String D = Long.toString(denominator);
-        return N + "/" + D;
+    public String toString() {
+        return (numerator) + "/" + (denominator);
     }
 
     // This method should always print the reduced form of the fraction.
     // If the fraction is negative, the sign should be displayed on the numerator, e.g., "-1/2" not "1/-2".
     // Note: once you implement this, you will be able to print a Fraction with println... more on how this works later.
     // Returns a (double precision) floating point number that is the approximate value of this fraction, printed as a real number.
-    double toDouble() {
+    public double toDouble() {
         double num = numerator;
         double den = denominator;
         return num / den;
--- CS6011/Week1/Day2/RainFall/src/RainData.java
+++ CS6011/Week1/Day2/RainFall/src/RainData.java
@@ -3,11 +3,13 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.io.FileNotFoundException;
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class RainData {
-//    String filename;
+    //    String filename;
 //    RainData(filename){
 //
 //    }
@@ -16,6 +18,7 @@ public class RainData {
 //        ArrayList<String> month = new ArrayList<>();
 //        ArrayList<Integer> year = new ArrayList<>();
 //        ArrayList<Double> rainfall = new ArrayList<>();
+
 
 
         try {
@@ -30,8 +33,20 @@ public class RainData {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-//        PrintWriter pw = new PrintWriter(newFileOutputStream("rainfall_results.txt"));
 
+        // calculate the average rainfall per month and store it in a variable
+
+//        try {
+//            FileWriter myWriter = new FileWriter("rainfall_results.txt");
+//            myWriter.write("Hello world");
+//            myWriter.close();
+//            System.out.println("Successfully wrote to the file.");
+//        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
+//    }
+//        PrintWriter pw = new PrintWriter(newFileOutputStream("rainfall_results.txt"));
 
 
     }
