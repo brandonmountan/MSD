74624d8db01d053a9b387dd4d2bdfe8ed6e0ead5
brandonmountan <brandonmountan1@gmail.com>
2024-09-25 09:16:48 -0600

    day 3 stuff

---
+++ CS6011/Week1/Day2/RainFall/src/RainData.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.FileNotFoundException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class RainData {
+//    String filename;
+//    RainData(filename){
+//
+//    }
+    public static void main(String[] args) {
+
+//        ArrayList<String> month = new ArrayList<>();
+//        ArrayList<Integer> year = new ArrayList<>();
+//        ArrayList<Double> rainfall = new ArrayList<>();
+
+
+        try {
+            File myFile = new File("rainfall_data.txt");
+            Scanner fileReader = new Scanner(myFile);
+            while (fileReader.hasNextLine()) {
+                String data = fileReader.nextLine();
+                System.out.println(data);
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+//        PrintWriter pw = new PrintWriter(newFileOutputStream("rainfall_results.txt"));
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MSD
+
+This is a collection of my homework assignments, labs, and projects from the Masters of Software Development Program at the University of Utah.
+
+CS6010 used C++ language and I wrote my code in Xcode. This was a 5 week course and it started with a simple "Hello World." program and ended with a group project that involved building a video game using the SFML library.
