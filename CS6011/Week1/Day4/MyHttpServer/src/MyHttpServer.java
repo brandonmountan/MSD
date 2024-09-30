31db47ff88812c80dfd32f52837d8f6924d6f4d9
brandonmountan <brandonmountan1@gmail.com>
2024-09-30 08:46:15 -0600

    thought 404 was working, it wasn't so now it's working

--- CS6011/Week1/Day4/MyHttpServer/src/MyHttpServer.java
+++ CS6011/Week1/Day4/MyHttpServer/src/MyHttpServer.java
@@ -12,18 +12,18 @@ public class MyHttpServer {
             while (true) {
                 // creat connection between server and client
                 try (Socket clientSocket = serverSocket.accept();
-                     // buffered reader reads text from a character-input stream (from the client)
-                     BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                     // PrintWriter is for output streams
-                     PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true)) {
-                    // create string object that stores the line of input that is read
-                    String requestLine = input.readLine();
+                     // buffered reader reads text from a character-httpRequest stream (from the client)
+                     BufferedReader httpRequest = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                     // PrintWriter is for httpResponse streams
+                     PrintWriter httpResponse = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                    // create string object that stores the line of httpRequest that is read
+                    String requestLine = httpRequest.readLine();
                     // as long as the request line is not blank or in other words as long as there is a request
                     if (requestLine != null) {
-                        // using regular expressions to separate the request line word by wored
+                        // using regular expressions to separate the request line word by word
                         String[] parts = requestLine.split(" ");
                         // the length of the array of words of the request needs to be greater than or equal to two
-                        // with the first part being the method (GET) and the second part is the path?
+                        // with the first part being the method (GET) and the second part is the path
                         if (parts.length >= 2 && parts[0].equals("GET") && parts[1].equals("/")) {
                             // create file variable from File class that specifies path
                             File file = new File("./resources/index.html");
@@ -31,29 +31,34 @@ public class MyHttpServer {
                             if (file.exists()) {
                                 // open connection to my file (index.html)
                                 try (FileInputStream fileInputStream = new FileInputStream(file)) {
-                                    // parse out the data
+                                    // parse out the data into bytes
                                     byte[] fileData = new byte[(int) file.length()];
                                     //
                                     fileInputStream.read(fileData);
                                     // send response using headers
-                                    output.println("HTTP/1.1 200 OK");
-                                    output.println("Content-Type: text/html");
-                                    output.println("Content-Length: " + fileData.length);
+                                    httpResponse.println("HTTP/1.1 200 OK");
+                                    httpResponse.println("Content-Type: text/html");
+                                    httpResponse.println("Content-Length: " + fileData.length);
 
-                                    output.println(); // Blank line to separate headers from body
-                                    output.flush(); // flush to make sure data is actually sent out over the network
+                                    httpResponse.println(); // Blank line to separate headers from body
+                                    httpResponse.flush(); // flush to make sure data is actually sent out over the network
 
                                     clientSocket.getOutputStream().write(fileData);
                                 } catch (IOException e) {
+                                    // catch if error with httpRequest or httpResponse stream dealing with file
                                     System.out.println("Error: " + e);
                                 }
-                            } else {
-                                output.println("HTTP/1.1 404 Not Found");
-                                output.println();
                             }
+                        } else {
+                            // 404 message if request is anything other than "/" for example /home
+                            httpResponse.println("HTTP/1.1 404 Not Found");
+                            httpResponse.println();
+                            httpResponse.flush();
+                            clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found".getBytes());
                         }
                     }
                 } catch (IOException e) {
+                    // catch if error with input or output stream
                     System.out.println("Error: " + e);
                 }
             }
